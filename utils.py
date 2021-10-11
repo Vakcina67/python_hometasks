6bf31a770e23a0723a7998a329b688c6e169689e
Evgeniy <evgechernykh@gmail.com>
2021-10-11 20:55:25 +0300

    task_4_and_5

---
+++ utils.py
@@ -0,0 +1,7 @@
+from Chernykh_Evgeniy_dz_4_2 import currency_rates
+
+currency_rates(input('Введите название валюты: '))
+if __name__ == '__main__':
+    import sys
+
+    result = currency_rates(sys.argv[0])
